12335837465e3a9f9d9081ab54ef6370f472781e
Frank Robert <frankie.robert@gmail.com>
2018-03-11 17:33:46 -0400

    add additional story for knob

--- src/components/Knob/Knob-story.jsx
+++ src/components/Knob/Knob-story.jsx
@@ -10,4 +10,10 @@ stories.addDecorator(withKnobs);
 stories
   .add('with divisions', () => <Knob divisions={4} />)
   .add('with offset', () => <Knob degreeOffset={45} />)
-  .add('with limited range', () => <Knob degreeRange={180} degreeOffset={90} />);
+  .add('with limited range', () => <Knob degreeRange={180} degreeOffset={90} />)
+  .add('side by side', () => (
+    <div>
+      <Knob degreeOffset={45} degreeRange={270} divisions={6} />
+      <Knob degreeRange={180} degreeOffset={90} divisions={3} />
+    </div>
+  ));
