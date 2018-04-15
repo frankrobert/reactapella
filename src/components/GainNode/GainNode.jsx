39c80b59de7a9a4ddb49d8dd53300d31fffbbc0e
Frank Robert <frankie.robert@gmail.com>
2018-04-15 10:29:08 -0400

    feat: strip out functionalities from Gain and StereoPanner, leveraging the HCO

--- src/components/GainNode/GainNode.jsx
+++ src/components/GainNode/GainNode.jsx
@@ -8,42 +8,21 @@ class Gain extends Component {
     audioContext: PropTypes.object
   };
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (
-      nextProps.audioContext &&
-      nextProps.currentNode &&
-      !prevState.audioNode
-    ) {
-      const audioNode = new GainNode(nextProps.audioContext, nextProps.options);
-      const value = nextProps.initialValue || 100;
-
-      audioNode.gain.setValueAtTime(value / 100, 0);
-
-      return { audioNode, value };
-    }
-
-    return null;
-  }
-
-  state = {
-    audioNode: null
-  };
-
-  onChange = (value, oldRange, param) => {
+  onChange = (value, oldRange, param = 'gain') => {
     const { audioContext } = this.props;
     const { audioNode } = this.state;
 
     this.setState({ [param]: value });
 
-    let newValue = value / 100;
+    let computedValue = value / 100;
 
-    if (newValue === 0) newValue = 0.01;
+    if (computedValue === 0) computedValue = 0.01;
 
-    audioNode.gain.exponentialRampToValueAtTime(
-      newValue,
+    audioNode[param].exponentialRampToValueAtTime(
+      computedValue,
       audioContext.currentTime + FUTURE_EVENT
     );
   };
 }
 
-export default createAudioNode(Gain);
+export default createAudioNode(Gain, GainNode);
